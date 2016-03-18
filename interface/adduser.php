d1d5192a52c2c46a9bc1b28addc3e6d9f9e5c3a4
pankpinther <dvdseroney@gmail.com>
2016-03-18 18:11:13 -0400

    add file adduser.php

---
+++ interface/adduser.php
@@ -0,0 +1,18 @@
+<html>
+<form action="adduser.php">
+  First name:<br>
+  <input type="text" value=""><br>
+  Other names:<br>
+  <input type="text" name="onames" value=""><br><br>
+Course:<br>
+  <input type="text" name="course" value=""><br><br>
+D.O.B:<br>
+  <input type="text" name="dob" value=""><br><br>
+Email:<br>
+  <input type="text" name="email" value=""><br><br>
+Password:<br>
+  <input type="text" name="password" value=""><br><br>
+
+  <input type="submit" value="Submit">
+</form>
+</html>
